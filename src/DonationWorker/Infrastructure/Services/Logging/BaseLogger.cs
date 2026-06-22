0ee31f913b89f06d4aa795ca2019ca6daa722373
Gustavo Merendi <gmerendi@hotmail.com>
2026-06-22 11:11:27 -0700

    Fixing Baselog

--- src/DonationWorker/Infrastructure/Services/Logging/BaseLogger.cs
+++ src/DonationWorker/Infrastructure/Services/Logging/BaseLogger.cs
@@ -74,7 +74,7 @@ namespace DonationWorker.Infrastructure.Services.Logging
 
         private void SendToDynamoDb(string logLevel, BaseLogType type, string message, object? data, string correlationId)
         {
-            if (_configuration["CustomLogging:SendLogToDB"] != "True") return;
+            if (type != BaseLogType.EVENT && _configuration["CustomLogging:SendLogToDB"] != "True") return;
 
             try
             {
